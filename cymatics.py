373047379156d2627a2119178146d337daf09c55
AStarfinger <Adrian.starfinger@web.de>
2020-08-30 09:56:34 +0200

    removing matplotlib from imports

--- cymatics.py
+++ cymatics.py
@@ -2,27 +2,11 @@ import numpy as np
 from PIL import Image
 import time
 import random
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+# import matplotlib.pyplot as plt
+# from matplotlib.animation import FuncAnimation
 from math import *
 import subprocess
 
-
-subprocess.run(["rm","pictures/cymatics","-r"])
-subprocess.run(["mkdir","pictures/cymatics"])
-width, height = 1500,1000
-data = None	
-i=5
-# for j in range(100):
-data = np.zeros((height, width,3), dtype=np.uint8)
-
-# data[0:2000:i, 0:3000] = [255, 255, 255] # red patch in upper left
-# img = Image.fromarray(data, 'RGBA')
-# img.save('pictures/cymatics/my.png')
-# img.show()
-i+=10
-# time.sleep(2)
-alpha = np.array(80,dtype=np.uint8)
 class Particle:
 	def __init__(self,x,y):
 		self.x = x
@@ -30,12 +14,6 @@ class Particle:
 		# self.pos = np.array((x,y))
 		self.vel = np.array((0,0))
 
-
-particles = []
-
-num_particles = 10000
-for i in range(num_particles):
-	particles.append(Particle(random.uniform(-width/2,width/2),random.uniform(-height/2,height/2)))
 
 def updateData():
 	global alpha
@@ -64,12 +42,38 @@ def updateData():
 
 
 
+
+subprocess.run(["rm","pictures/cymatics","-r"])
+subprocess.run(["mkdir","pictures/cymatics"])
+width, height = 1500,1000
+data = None	
+i=5
+# for j in range(100):
+data = np.zeros((height, width,3), dtype=np.uint8)
+
+# data[0:2000:i, 0:3000] = [255, 255, 255] # red patch in upper left
+# img = Image.fromarray(data, 'RGBA')
+# img.save('pictures/cymatics/my.png')
+# img.show()
+i+=10
+# time.sleep(2)
+alpha = np.array(80,dtype=np.uint8)
+
 # plt.plot(data)
 # plt.show()
 # fig = plt.figure(figsize=(6,6), facecolor='black')
 # im = plt.imshow(data)
 running = True
-num_iterations = 100000
+num_iterations = 200
+num_particles = 100000
+
+
+particles = []
+
+for i in range(num_particles):
+	particles.append(Particle(random.uniform(-width/2,width/2),random.uniform(-height/2,height/2)))
+
+
 magnitude = 10
 random_number= random.random()*magnitude-magnitude/2
 random_number2= random.random()*magnitude-magnitude/2
